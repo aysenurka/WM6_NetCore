0f265902b91872f96d42dd206810f519da898b7f
Ayşenur Kara <aysenurk.95@outlook.com>
2019-03-08 19:43:42 +0300

    rename project

---
+++ CoreIdentity/Models/IdentityModels/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace CoreIdentity.Models.IdentityModels
+{
+    public class ApplicationRole : IdentityRole
+    {
+        [Required, StringLength(128)]
+        public string Description { get; set; }
+    }
+}
---
+++ CoreIdentity/Models/IdentityModels/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace CoreIdentity.Models.IdentityModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+        [Required, StringLength(50)]
+        public string Surname { get; set; }
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+    }
+}
