a67892f2a4ca79b97cdb09ec90620607bb99bcac
Ayşenur Kara <aysenurk.95@outlook.com>
2019-03-19 22:47:53 +0300

    entity classları ve mycontext oluşturuldu

---
+++ Kuzey.Models/IdentityEntities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kuzey.Models.IdentityEntities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, StringLength(50)] public string Name { get; set; } = "Test";
+        [Required, StringLength(50)] public string Surname { get; set; } = "Test2";
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+    }
+}
