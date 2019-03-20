1568edab036ae3a57c6fa5253103c92dae625ff5
Ayşenur Kara <aysenurk.95@outlook.com>
2019-03-20 19:54:44 +0300

    generic repository oluşturuldu

---
+++ Kuzey.BLL/Repository/Abstracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Kuzey.Models.Entities;
+using System;
+using System.Linq;
+
+namespace Kuzey.BLL.Repository.Abstracts
+{
+    public interface IRepository<T, TId> where T : BaseEntity<TId>
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Func<T, bool> predicate);
+        T GetById(TId id);
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        void Save();
+    }
+}
